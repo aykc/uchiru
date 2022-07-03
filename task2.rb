f38dc916e6f5cb250c7d8be7f0b5a60d26a355bc
Anton Berkutov <void__fastcall@mail.ru>
2022-07-03 18:28:46 +0300

    Change map&flatten methods to flat_map

--- task2.rb
+++ task2.rb
@@ -8,8 +8,8 @@
 
 arr = [{ a: 1, b: 2, c: 45 }, { d: 123, c: 12 }, { e: 87 }]
 
-p "All keys: #{arr.map(&:keys).flatten.uniq}"
+p "All keys: #{arr.flat_map(&:keys).uniq}"
 
-p "All values: #{arr.map(&:values).flatten}"
+p "All values: #{arr.flat_map(&:values)}"
 
-p "Sum of all values: #{arr.map(&:values).flatten.reduce(:+)}"
+p "Sum of all values: #{arr.flat_map(&:values).reduce(:+)}"
